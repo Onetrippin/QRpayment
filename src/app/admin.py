06d9704b3eb4a76161083271a7a7dfdacb340b44
Onetrippin <kolimpiadnik@gmail.com>
2025-04-04 00:08:28 +0500

    added docker, docker-compose, makefile and some more different things

---
+++ src/app/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from app.internal.admin.admin_user import AdminUserAdmin
+
+admin.site.site_title = 'QR-payment administration'
+admin.site.site_header = 'QR-payment administration'
