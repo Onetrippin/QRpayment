8ee5c1426999c9a09b8d415ad97ce571e8c5a39c
Onetrippin <kolimpiadnik@gmail.com>
2025-06-03 05:57:08 +0500

    changed api path

--- src/config/urls.py
+++ src/config/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [path('admin/', admin.site.urls), path('api/', include('app.internal.api'))] + (
+urlpatterns = [path('admin/', admin.site.urls), path('', include('app.internal.api'))] + (
     static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 )
